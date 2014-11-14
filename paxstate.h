024c16b0c205d5f7b5758f620f83d5ee05dd9e27
chengfu16 <chengfu16@gmail.com>
2014-11-14 15:26:07 -0600

    removed statc keywords

--- paxstate.h
+++ paxstate.h
@@ -1,27 +1,27 @@
-class learner_state {
+class learner {
   public:
     typedef struct learner_record_t {
       int         iid;
       int         ballot;
       int         proposer_id;
-      char*       final_value;
+      char *      final_value;
       int         final_value_size;
       learn_msg*  learns[N_OF_ACCEPTORS];
     } learner_record;
 
-    static unsigned int quorum;
+    unsigned int quorum;
 
-    static int highest_delivered = -1;
-    static int highest_seen = -1;
-    static learner_record learner_array[LEARNER_ARRAY_SIZE];
-    static int learner_ready = 0;
+    int highest_delivered = -1;
+    int highest_seen = -1;
+    learner_record learner_array[LEARNER_ARRAY_SIZE];
+    int learner_ready = 0;
 };
 
-class leader_state {
+class leader {
   public:
-    static int quorum;
-    static int proposer_id;
-    static int current_iid = 0;
+    int quorum;
+    int proposer_id;
+    int current_iid = 0;
     typedef struct phase1_info_t {
       int pending_count;
       int ready_count;
@@ -29,15 +29,15 @@ class leader_state {
       int first_to_check;
       int last_to_check;
     } phase1_info;
-    static phase1_info p1info;
+    phase1_info p1info;
 
     typedef struct phase2_info_t {
       int current_iid;
     } phase2_info;
-    static phase2_info p2info;
+    phase2_info p2info;
 
-    static char leader_send_buffer[MAX_UDP_MSG_SIZE];
-    static paxos_msg* msg = (paxos_msg*) leader_send_buffer;
+    char leader_send_buffer[MAX_UDP_MSG_SIZE];
+    paxos_msg* msg = (paxos_msg*) leader_send_buffer;
 
     typedef enum status_flag_t {
       p1_new,
@@ -62,25 +62,25 @@ class leader_state {
       // promise_msg* reserved_promise;
     } proposer_record;
 
-    static proposer_record proposer_array[PROPOSER_ARRAY_SIZE];
+    proposer_record proposer_array[PROPOSER_ARRAY_SIZE];
 };
 
-class proposer_state {
+class proposer {
   public:
-    static int fixed_ballot;
-    static int proposer_id;
-    static struct timeval proposer_to_interval;
+    int fixed_ballot;
+    int proposer_id;
+    struct timeval proposer_to_interval;
 
     //Lock
-    static int client_waiting = 0;
-    static int current_iid;
-    static int value_delivered;
-    static int last_accept_iid;
-    static int last_accept_hash;
+    int client_waiting = 0;
+    int current_iid;
+    int value_delivered;
+    int last_accept_iid;
+    int last_accept_hash;
     //Lock
 
-    static char proposer_send_buffer[MAX_UDP_MSG_SIZE];
-    static int msg_size;
+    char proposer_send_buffer[MAX_UDP_MSG_SIZE];
+    int msg_size;
     typedef struct timeout_info_t {
       int instance_id;
       int hash;
